b2bf22e4d42ae7e406cac966550b0d9f2c580c54
Osher Lerner <oshlern@nuevaschool.org>
2017-02-22 14:12:15 -0800

    added backward consistency, dinalization and bug fixes to motor segments

--- src/io/getcoffee/motionprofiles/MotionTrajectory.java
+++ src/io/getcoffee/motionprofiles/MotionTrajectory.java
@@ -3,7 +3,6 @@ package io.getcoffee.motionprofiles;
 
 import java.util.LinkedList;
 import java.util.Map;
-import io.getcoffee.motionprofiles.WheelTrajectory.Wheel;
 
 strictfp public class MotionTrajectory {
 	public static final double robotMaxVel = MotionTrajectoryExecutor.robotMaxVel;
@@ -13,6 +12,7 @@ strictfp public class MotionTrajectory {
 	protected final double tickTime, tickTotal;
 	protected final WheelTrajectory leftWheel, rightWheel;
 	protected final LinkedList<SplineSegment> featureSegments;
+	protected final LinkedList<MotionTrajectorySegment> trajectorySegments;
 	/**
 	 * Maps a tick to a left/right-specific segment, and the time that the tick occurs at relative to
 	 * the beginning time of the segment.
@@ -36,27 +36,76 @@ strictfp public class MotionTrajectory {
 		this.tickTotal = tickTotal;
 		// TODO: Update the threshold to reflect a real value.
 		featureSegments = splineGenerator.generateFeatureSegments(0.1);
-		leftWheel = new WheelTrajectory(this, featureSegments, Wheel.LEFT, tickTotal, tickTime);
-		rightWheel = new WheelTrajectory(this, featureSegments, Wheel.RIGHT, tickTotal, tickTime);
+		trajectorySegments = finalizeSegments(
+			generateBackwardConsistency(generateSegmentsAndForwardConsistency(featureSegments)));
 		System.out.println(featureSegments.size());
-		leftWheelTickMap = leftWheel.generateTickMap();
-		rightWheelTickMap = rightWheel.generateTickMap();
+		generateTickMap();
 	}
 
-	public LinkedList<MotionTrajectorySegment> generateSegmentsAndForwardPass(LinkedList<SplineSegment> featureSegments) {
+	/**
+	 * Generates a 'forward consistent' set of segments for each spline feature segment. This ensures that
+	 * the movement follows the acceleration constraints of both the robot and path.
+	 * 
+	 * The segments that this returns are not ready to be used and should be run through the backward consistency
+	 * filter and afterwards finalized.
+	 * 
+	 * @see {@link MotionTrajectory#generateBackwardConsistency}
+	 * @see {@link MotionTrajectory#finalizeSegments}
+	 * 
+	 * @param featureSegments
+	 * @return ordered right/left trajectory segments that are forward consistent.
+	 */
+	public LinkedList<MotionTrajectorySegment> generateSegmentsAndForwardConsistency(
+		LinkedList<SplineSegment> featureSegments) {
 		LinkedList<MotionTrajectorySegment> trajectorySegments = new LinkedList<>();
-		SplineSegment featureSegment = featureSegments.get(0);
-		Tuple<Double, Double> velAndAccel = calcMaxVelAndAccFromCurvatureAndDerivative(featureSegment.maxCurvature,
-			featureSegment.maxDCurvature);
 		MotionTrajectorySegment lastSegment = new MotionTrajectorySegment(0.0); // Initialize velocity at 0
-		for (int i = 0; i < featureSegments.size(); i++) {
-			featureSegment = featureSegments.get(i);
-			lastSegment = new MotionTrajectorySegment(featureSegment.length, lastSegment.finVel, velAndAccel.getX(),
-				velAndAccel.getY());
-			velAndAccel = calcMaxVelAndAccFromCurvatureAndDerivative(featureSegment.maxCurvature, featureSegment.maxDCurvature);
-			lastSegment.finVel = Math.min(lastSegment.calcReachableEndVel(), Math.min(lastSegment.maxVel, velAndAccel.getX()));
+		Tuple<Double, Double> maxVelAndAccel = calcMaxVelAndAccFromCurvatureAndDerivative(featureSegments.get(0).maxCurvature,
+			featureSegments.get(0).maxDCurvature);
+		for (SplineSegment featureSegment : featureSegments) {
+			lastSegment = new MotionTrajectorySegment(featureSegment.length, lastSegment.finVel, maxVelAndAccel.getX(),
+				maxVelAndAccel.getY());
+			maxVelAndAccel = calcMaxVelAndAccFromCurvatureAndDerivative(featureSegment.maxCurvature,
+				featureSegment.maxDCurvature);
+			lastSegment.finVel = Math.min(lastSegment.calcReachableEndVel(),
+				Math.min(lastSegment.maxVel, maxVelAndAccel.getX()));
 			trajectorySegments.add(lastSegment);
 		}
+	}
+
+	/**
+	 * Generates a 'backward consistent' set of segments from a 'forward consistent' set of segments. Establishing
+	 * backwards consistency ensures the segments obey deceleration constraints.
+	 * 
+	 * @param trajectorySegments
+	 *        ordered segments that are forward consistent
+	 * @return ordered right/left trajectory segments that are forward and backward consistent.
+	 */
+	public LinkedList<MotionTrajectorySegment> generateBackwardConsistency(
+		LinkedList<MotionTrajectorySegment> trajectorySegments) {
+		for (int i = trajectorySegments.size() - 1; i > 0; i--) {
+			MotionTrajectorySegment trajectorySegment = trajectorySegments.get(i);
+			trajectorySegment.initVel = Math.min(trajectorySegment.calcReachableStartVel(), trajectorySegment.initVel);
+		}
+		return trajectorySegments;
+	}
+
+	/**
+	 * Attach an absolute context and finalize the path of each of the segments.
+	 * 
+	 * @param trajectorySegments
+	 *        ordered segments that are forward and backward consistent.
+	 * @return finalized ordered right/left trajectory segments with absolute context attached.
+	 */
+	public LinkedList<MotionTrajectorySegment> finalizeSegments(LinkedList<MotionTrajectorySegment> trajectorySegments) {
+		double timePassed = 0;
+		double distanceTraveled = 0;
+		for (MotionTrajectorySegment segment : trajectorySegments) {
+			segment.dividePath();
+			segment.context = new AbsoluteSegmentContext(timePassed, distanceTraveled);
+			timePassed += segment.duration;
+			distanceTraveled += segment.length;
+		}
+		return trajectorySegments;
 	}
 
 	/**
@@ -73,7 +122,8 @@ strictfp public class MotionTrajectory {
 	public Tuple<MotionTrajectoryPoint, MotionTrajectoryPoint> calcPoint(int tick) {
 		Tuple<Double, WheelTrajectorySegment> leftWheelTick = leftWheelTickMap.get(tick);
 		Tuple<Double, WheelTrajectorySegment> rightWheelTick = rightWheelTickMap.get(tick);
-		return new Tuple<>(leftWheelTick.getY().findSetPoint(leftWheelTick.getX(), tick),
+		return new Tuple<MotionTrajectoryPoint, MotionTrajectoryPoint>(
+			leftWheelTick.getY().findSetPoint(leftWheelTick.getX(), tick),
 			rightWheelTick.getY().findSetPoint(rightWheelTick.getX(), tick));
 	}
 
@@ -89,7 +139,7 @@ strictfp public class MotionTrajectory {
 	public Tuple<Double, Double> calcMaxAccelFromCurvatureAndDerivative(double curvature, double dCurvature) {
 		double divisor = (1 + plantWidth * Math.abs(curvature));
 		double maxVel = robotMaxVel / divisor;
-		return Tuple(maxVel, (robotMaxAccel - plantWidth * maxVel * dCurvature) / divisor);
+		return new Tuple<Double, Double>(maxVel, (robotMaxAccel - plantWidth * maxVel * dCurvature) / divisor);
 	}
 
 	public double calcMaxSpeed(double s) {
@@ -97,7 +147,7 @@ strictfp public class MotionTrajectory {
 	}
 
 	public double calcMaxSpeedFromCurvature(double curvature) {
-		return maxVel / (1 + plantWidth * Math.abs(curvature) / 2);
+		return robotMaxVel / (1 + plantWidth * Math.abs(curvature) / 2);
 	}
 
 	public double calcMaxAngularVel(double s) {
@@ -118,9 +168,9 @@ strictfp public class MotionTrajectory {
 		double splineSpeed = Math.sqrt(splineVel.getX() * splineVel.getX() + splineVel.getY() * splineVel.getY());
 		double splineSpeedD = (splineAcc.getX() * splineVel.getX() + splineAcc.getY() * splineVel.getY())
 			/ (splineSpeed * splineSpeed); // The derivative of the speed of the spline / v
-		Tuple<Double, Double> perpDerivative = new Tuple<>(
+		Tuple<Double, Double> perpDerivative = new Tuple<Double, Double>(
 			(splineVel.getY() * splineSpeedD - splineAcc.getY()) / splineSpeed * plantWidth / 2.0,
 			(splineAcc.getX() - splineVel.getX() * splineSpeedD) / splineSpeed * plantWidth / 2.0);
-		return new Tuple<>(perpDerivative, splineVel);
+		return new Tuple<Tuple<Double, Double>, Tuple<Double, Double>>(perpDerivative, splineVel);
 	}
 }
