a5681d45c74848f51ea5b4d98ce29170c622b253
Osher Lerner <oshlern@nuevaschool.org>
2017-02-26 20:55:47 -0800

    organized and changed executor

--- src/io/getcoffee/motionprofiles/MotionTrajectoryExecutor.java
+++ src/io/getcoffee/motionprofiles/MotionTrajectoryExecutor.java
@@ -7,52 +7,37 @@ strictfp public class MotionTrajectoryExecutor {
 
 	public static void main(String[] args) {
 		SplineGenerator spline = new QuinticSplineGenerator(
+			// xi, yi, xf, yf
 			// Position
 			0, 0, 1, 2,
 			// Velocity
 			1, 0, 0, 1,
 			// Acceleration
 			1, 0, 0, -20);
-		System.out.println(spline.calcPos(0.6));
-		System.out.println(spline.calcVel(0.6));
-		System.out.println(spline.calcAcc(0.6));
-		System.out.println(spline.calcSpeed(0.6));
-		System.out.println(spline.calcCurvature(0.6));
-		System.out.println("##############################");
-		MotionTrajectory motionTrajectory = new MotionTrajectory(spline, 0.5, 10);
-		System.out.println(motionTrajectory.featureSegments.size());
-		System.out.println(motionTrajectory.rightWheel.trajectorySegments.size());
-		System.out.println(motionTrajectory.leftWheel.trajectorySegments.size());
-		System.out.println("##############################");
-		double durationTallyRight = 0;
-		double durationTallyLeft = 0;
-		double lengthTallyRight = 0;
-		double lengthTallyLeft = 0;
-		for (int i = 0; i < motionTrajectory.featureSegments.size() - 1; i++) {
-			System.out.println("RIGHT{" + i + "}" + motionTrajectory.rightWheel.trajectorySegments.get(i).duration + " LEFT{"
-				+ i + "}" + motionTrajectory.leftWheel.trajectorySegments.get(i).duration + " DIF # "
-				+ (motionTrajectory.rightWheel.trajectorySegments.get(i).duration
-					- motionTrajectory.leftWheel.trajectorySegments.get(i).duration));
-			// System.out.println(motionTrajectory.leftWheel.trajectorySegments.get(i));
-			durationTallyRight += motionTrajectory.rightWheel.trajectorySegments.get(i).duration;
-			durationTallyLeft += motionTrajectory.leftWheel.trajectorySegments.get(i).duration;
-			lengthTallyRight += motionTrajectory.rightWheel.trajectorySegments.get(i).length;
-			lengthTallyLeft += motionTrajectory.leftWheel.trajectorySegments.get(i).length;
-		}
-		System.out.println("RIGHT DUR: " + (durationTallyRight / motionTrajectory.featureSegments.size()));
-		System.out.println("LEFT  DUR: " + (durationTallyLeft / motionTrajectory.featureSegments.size()));
-		System.out.println("RIGHT LEN: " + (lengthTallyRight / motionTrajectory.featureSegments.size()));
-		System.out.println("LEFT  LEN: " + (lengthTallyLeft / motionTrajectory.featureSegments.size()));
-		// System.out.println(motionTrajectory.calcPoint(0.8, lastPoint));
+		testSpline(0.6, spline);
+		MotionTrajectory motionTrajectory = new MotionTrajectory(spline, 0.5, 10); // where do we get tick time?
+		System.out.println("number of segments: " + motionTrajectory.featureSegments.size());
+		printPoints(motionTrajectory);
+	}
+
+	public static void printPoints(MotionTrajectory motionTrajectory) {
 		MotionTrajectoryPoint motionTrajectoryPoint = new MotionTrajectoryPoint(0, 0, 0, 0);
 		Tuple<MotionTrajectoryPoint, MotionTrajectoryPoint> lastPoint = new Tuple<>(motionTrajectoryPoint,
 			motionTrajectoryPoint);
-		for (int i = 1; i < 200; i++) {
-			System.out.println(lastPoint);
+		for (int i = 1; i < motionTrajectory.getTickTotal(); i++) { // What about the first setpoint?
+			System.out.println(lastPoint); // last point should be used as context for new point (makes program run better and more efficiently, alternative not implemented) (though honestly the contextualized method hasn't been implemented either) (well at least we thought about it a lot)
 			Tuple<MotionTrajectoryPoint, MotionTrajectoryPoint> newPoint = motionTrajectory.calcPoint(i);
 			lastPoint = newPoint;
 		}
 		System.out.println(lastPoint);
-		{}
+	}
+
+	public static void testSpline(double s, SplineGenerator spline) {
+		System.out.println("Spline point at " + s);
+		System.out.println("Pos " + spline.calcPos(s));
+		System.out.println("Vel " + spline.calcVel(s));
+		System.out.println("Acc " + spline.calcAcc(s));
+		System.out.println("Spd " + spline.calcSpeed(s));
+		System.out.println("Curv " + spline.calcCurvature(s));
 	}
 }
