3c79aff02bdafa074749d26a4eba4c59a1b9cfec
Howard Stark <howard@getcoffee.io>
2017-03-19 12:56:41 -0700

    Fixed duplicate maxAccel being passed in as minAccel

--- src/io/getcoffee/motionprofiles/MotionTrajectory.java
+++ src/io/getcoffee/motionprofiles/MotionTrajectory.java
@@ -58,10 +58,9 @@ strictfp public class MotionTrajectory {
 		double maxVel = calcMaxVel(firstFeature.maxCurve);
 		double lastFinVel = 0.0;
 		for (Map.Entry<Double, SplineSegment> featureEntry : featureSegments.entrySet()) {
-			System.out
-				.println("Max Accel: " + featureEntry.getValue().maxAcc + ", Min Accel: " + featureEntry.getValue().minAcc);
+			System.out.println("Max Accel: " + featureEntry.getValue().maxAcc + ", Min Accel: " + featureEntry.getValue().minAcc);
 			MotionTrajectorySegment segment = new MotionTrajectorySegment(featureEntry.getValue().length, lastFinVel, maxVel,
-				featureEntry.getValue().maxAcc, featureEntry.getValue().maxAcc);
+				featureEntry.getValue().maxAcc, featureEntry.getValue().minAcc);
 			maxVel = calcMaxVel(featureEntry.getValue().maxCurve);
 			segment.finVel = Math.min(segment.maxVel, maxVel);
 			trajectorySegments.add(segment);
