1b4df27556f79b5f24600a015561538104aa79e8
Howard Stark <howard@getcoffee.io>
2017-03-26 16:34:47 -0700

    Circular spine generator

---
+++ src/io/getcoffee/motionprofiles/CircleSplineGenerator.java
@@ -0,0 +1,71 @@
+package io.getcoffee.motionprofiles;
+
+/**
+ * Created by howard on 3/26/17.
+ */
+public class CircleSplineGenerator extends SplineGenerator {
+
+	public CircleSplineGenerator() {
+
+	}
+
+	@Override
+	protected void initializePos() {
+
+	}
+
+	@Override
+	protected double PosX(double s) {
+		return 0;
+	}
+
+	@Override
+	protected double PosY(double s) {
+		return 0;
+	}
+
+	@Override
+	protected void initializeVel() {
+
+	}
+
+	@Override
+	protected double VelX(double s) {
+		return 0;
+	}
+
+	@Override
+	protected double VelY(double s) {
+		return 0;
+	}
+
+	@Override
+	protected void initializeAcc() {
+
+	}
+
+	@Override
+	protected double AccX(double s) {
+		return 0;
+	}
+
+	@Override
+	protected double AccY(double s) {
+		return 0;
+	}
+
+	@Override
+	protected void initializeJerk() {
+
+	}
+
+	@Override
+	protected double JerkX(double s) {
+		return 0;
+	}
+
+	@Override
+	protected double JerkY(double s) {
+		return 0;
+	}
+}
