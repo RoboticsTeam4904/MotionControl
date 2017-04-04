f2f2aff54341ba3e03e44842bc9f8b73214b79dd
Osher Lerner <oshlern@nuevaschool.org>
2017-04-03 23:42:29 -0700

    added methods for circular path. Unfinished: separate from splines and figure out radius

--- src/io/getcoffee/motionprofiles/CircleSplineGenerator.java
+++ src/io/getcoffee/motionprofiles/CircleSplineGenerator.java
@@ -1,71 +1,68 @@
 package io.getcoffee.motionprofiles;
 
-/**
- * Created by howard on 3/26/17.
- */
-public class CircleSplineGenerator extends SplineGenerator {
 
-	public CircleSplineGenerator() {
+public class CircularSplineGenerator extends SplineGenerator {
+	private final double initPosX, initPosY, finPosX, finPosY, finAngle;
+	protected final double centerX, centerY, radius, initAngle;
+	protected final double absoluteLength;
 
+	CircularSplineGenerator(double initPosX, double initPosY, double finPosX, double finPosY, double finAngle) {
+		finAngle = finAngle - Math.PI / 2;
+		this.initPosX = initPosX;
+		this.initPosY = initPosY;
+		this.finPosX = finPosX;
+		this.finPosY = finPosY;
+		this.finAngle = finAngle;
+		this.radius = calcRadius(initPosX, initPosY, finPosX, finPosY, finAngle);
+		this.centerX = finPosX + radius * Math.cos(finAngle);
+		this.centerY = finPosY + radius * Math.sin(finAngle);
+		this.initAngle = Math.atan2(initPosX - centerX, initPosY - centerY);
+		// this.radius = Math.abs(radius)
+		this.absoluteLength = 2 * (finAngle - initAngle) * radius;
 	}
 
-	@Override
-	protected void initializePos() {
-
+	protected double calcRadius(double initPosX, double initPosY, double finPosX, double finPosY, double phi) {
+		return ((initPosX - finPosX) * (initPosX - finPosX) + (initPosY - finPosY) * (initPosY - finPosY))
+			/ (2 * (initPosX - finPosX));
 	}
 
-	@Override
-	protected double PosX(double s) {
-		return 0;
+	protected double timeToAngle(double t, double initAngle, double finAngle) {
+		return (finAngle - initAngle) * t + initAngle;
 	}
 
-	@Override
-	protected double PosY(double s) {
-		return 0;
+	protected double timeToAngle(double t) {
+		return timeToAngle(t, initAngle, finAngle);
 	}
 
-	@Override
-	protected void initializeVel() {
-
+	protected double PosX(double t) {
+		return centerX + radius * Math.cos(timeToAngle(t));
 	}
 
-	@Override
-	protected double VelX(double s) {
-		return 0;
+	protected double PosY(double t) {
+		return centerY + radius * Math.sin(timeToAngle(t));
 	}
 
-	@Override
-	protected double VelY(double s) {
-		return 0;
+	protected double VelX(double t) {
+		return -radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle);
 	}
 
-	@Override
-	protected void initializeAcc() {
-
+	protected double VelY(double t) {
+		return radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle);
 	}
 
-	@Override
-	protected double AccX(double s) {
-		return 0;
+	protected double AccX(double t) {
+		return -radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle);
 	}
 
-	@Override
-	protected double AccY(double s) {
-		return 0;
+	protected double AccY(double t) {
+		return -radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle);
 	}
 
-	@Override
-	protected void initializeJerk() {
-
+	protected double JerkX(double t) {
+		return radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle) * (finAngle - initAngle);
 	}
 
-	@Override
-	protected double JerkX(double s) {
-		return 0;
-	}
-
-	@Override
-	protected double JerkY(double s) {
-		return 0;
+	protected double JerkY(double t) {
+		return -radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle) * (finAngle - initAngle);
 	}
 }
