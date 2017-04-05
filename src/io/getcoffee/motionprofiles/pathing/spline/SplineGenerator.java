70b316bb91484ff19921ef8cdad2df0e32530320
Osher Lerner <oshlern@nuevaschool.org>
2017-04-05 08:53:36 -0700

    partially fixed problems with new splines. Derivative might work yet evaluate is jank

--- src/io/getcoffee/motionprofiles/pathing/spline/SplineGenerator.java
+++ src/io/getcoffee/motionprofiles/pathing/spline/SplineGenerator.java
@@ -16,6 +16,9 @@ strictfp public abstract class SplineGenerator extends io.getcoffee.motionprofil
 	 */
 	protected double[] derivative(double[] coefs) {
 		int length = coefs.length;
+		if (length == 1) {
+			return new double[] {0.0};
+		}
 		double[] dCoefs = new double[length - 1];
 		for (int i = 1; i < length; i++) {
 			dCoefs[i - 1] = coefs[i] * i;
