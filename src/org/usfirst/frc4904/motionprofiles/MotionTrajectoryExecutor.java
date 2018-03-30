8173fa527f843a2c023494a33235bbe9e8b52aa5
Osher Lerner <osherler@gmail.com>
2018-03-29 21:25:28 -0700

    fixing circular splines

--- src/org/usfirst/frc4904/motionprofiles/MotionTrajectoryExecutor.java
+++ src/org/usfirst/frc4904/motionprofiles/MotionTrajectoryExecutor.java
@@ -1,33 +1,37 @@
 package org.usfirst.frc4904.motionprofiles;
 
-
+import org.usfirst.frc4904.motionprofiles.pathing.CirclePathGenerator;
 import org.usfirst.frc4904.motionprofiles.pathing.PathGenerator;
-import org.usfirst.frc4904.motionprofiles.pathing.spline.QuinticSplineGenerator;
 
 strictfp public class MotionTrajectoryExecutor {
 	public static final double robotMaxVel = 5;
 	public static final double robotMaxAccel = 1;
-	public static final double plantWidth = 0.25; //radius
+	public static final double plantWidth = 0.25; // radius
 
 	public static void main(String[] args) {
-		PathGenerator spline = new QuinticSplineGenerator(
-			// xi, yi, xf, yf
-			// Position
-			0, 0, 2, 0,
-			// Velocity
-			1, 0, 0, 0,
-			// Acceleration
-			1, 0, -1, 0);
+		// PathGenerator spline = new QuinticSplineGenerator(
+		// // xi, yi, xf, yf
+		// // Position
+		// 0, 0, 2, 0,
+		// // Velocity
+		// 1, 0, 0, 0,
+		// // Acceleration
+		// 1, 0, -1, 0);
+		PathGenerator spline = new CirclePathGenerator(0, 0, 1, 1, 0.1);
 		testSpline(0.6, spline);
-		MotionTrajectory motionTrajectory = new MotionTrajectory(spline, plantWidth, 10);
-		printPoints(motionTrajectory);
+		// MotionTrajectory motionTrajectory = new MotionTrajectory(spline,
+		// plantWidth, 10);
+		// printPoints(motionTrajectory);
 	}
 
 	public static void printPoints(MotionTrajectory motionTrajectory) {
 		MotionTrajectoryPoint motionTrajectoryPoint = new MotionTrajectoryPoint(0, 0, 0, 0);
 		Tuple<MotionTrajectoryPoint, MotionTrajectoryPoint> lastPoint = new Tuple<>(motionTrajectoryPoint,
-			motionTrajectoryPoint);
-		for (int i = 1; i < motionTrajectory.getTickTotal(); i++) { // What about the first setpoint?
+				motionTrajectoryPoint);
+		for (int i = 1; i < motionTrajectory.getTickTotal(); i++) { // What
+																	// about the
+																	// first
+																	// setpoint?
 			Tuple<MotionTrajectoryPoint, MotionTrajectoryPoint> newPoint = motionTrajectory.calcPoint(i, lastPoint);
 			lastPoint = newPoint;
 		}
--- src/org/usfirst/frc4904/motionprofiles/pathing/CirclePathGenerator.java
+++ src/org/usfirst/frc4904/motionprofiles/pathing/CirclePathGenerator.java
@@ -1,13 +1,12 @@
 package org.usfirst.frc4904.motionprofiles.pathing;
 
-
 strictfp public class CirclePathGenerator extends PathGenerator {
 	private final double initPosX, initPosY, finPosX, finPosY, finAngle;
 	protected final double centerX, centerY, radius, initAngle;
 	protected final double absoluteLength;
 
-	CirclePathGenerator(double initPosX, double initPosY, double finPosX, double finPosY, double finAngle) {
-		finAngle = finAngle - Math.PI / 2;
+	public CirclePathGenerator(double initPosX, double initPosY, double finPosX, double finPosY, double finAngle) {
+		finAngle = finAngle + Math.PI / 2; // Only works if we are turning left
 		this.initPosX = initPosX;
 		this.initPosY = initPosY;
 		this.finPosX = finPosX;
@@ -16,14 +15,24 @@ strictfp public class CirclePathGenerator extends PathGenerator {
 		this.radius = calcRadius(initPosX, initPosY, finPosX, finPosY, finAngle);
 		this.centerX = finPosX + radius * Math.cos(finAngle);
 		this.centerY = finPosY + radius * Math.sin(finAngle);
-		this.initAngle = Math.atan2(initPosX - centerX, initPosY - centerY);
+		this.initAngle = Math.atan2(initPosY - centerY, initPosX - centerX);
 		this.absoluteLength = 2 * (finAngle - initAngle) * radius;
+		// System.out.println("Radius, " + Double.toString(this.radius) + "
+		// CenterX, " + Double.toString(this.centerX)
+		// + " CenterY, " + Double.toString(this.centerY));
+		// System.out.println(Double.toString(this.initAngle) + " init, fin " +
+		// Double.toString(this.finAngle));
+		// double a = 0.6;
+		// System.out.println("angle, " + Double.toString(timeToAngle(a)) + " x,
+		// "
+		// + Double.toString(radius * Math.cos(timeToAngle(a))) + " y, "
+		// + Double.toString(radius * Math.sin(timeToAngle(a))));
 		initialize();
 	}
 
 	protected double calcRadius(double initPosX, double initPosY, double finPosX, double finPosY, double phi) {
 		return ((initPosX - finPosX) * (initPosX - finPosX) + (initPosY - finPosY) * (initPosY - finPosY))
-			/ (2 * ((initPosX - finPosX) * Math.cos(phi) + (initPosY - finPosY) * Math.sin(phi)));
+				/ (2 * ((initPosX - finPosX) * Math.cos(phi) + (initPosY - finPosY) * Math.sin(phi)));
 	}
 
 	protected double timeToAngle(double t, double initAngle, double finAngle) {
@@ -35,34 +44,36 @@ strictfp public class CirclePathGenerator extends PathGenerator {
 	}
 
 	protected double PosX(double t) {
-		return centerX + radius * Math.cos(timeToAngle(t));
+		return centerX - radius * Math.cos(timeToAngle(t));
 	}
 
 	protected double PosY(double t) {
-		return centerY + radius * Math.sin(timeToAngle(t));
+		return centerY - radius * Math.sin(timeToAngle(t));
 	}
 
 	protected double VelX(double t) {
-		return -radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle);
+		return radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle);
 	}
 
 	protected double VelY(double t) {
-		return radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle);
+		return -radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle);
 	}
 
 	protected double AccX(double t) {
-		return -radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle);
+		return radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle);
 	}
 
 	protected double AccY(double t) {
-		return -radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle);
+		return radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle);
 	}
 
 	protected double JerkX(double t) {
-		return radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle) * (finAngle - initAngle);
+		return -radius * Math.sin(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle)
+				* (finAngle - initAngle);
 	}
 
 	protected double JerkY(double t) {
-		return -radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle) * (finAngle - initAngle);
+		return radius * Math.cos(timeToAngle(t)) * (finAngle - initAngle) * (finAngle - initAngle)
+				* (finAngle - initAngle);
 	}
 }
